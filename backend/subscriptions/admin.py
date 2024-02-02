2daa461afa57940abb152abdb9f39b83300fcbfc
Semenov Yuriy <info@juriys.ru>
2024-02-02 18:17:24 +0300

    [+] Добавлена подписка на блоги
    R:Нужна было реализовать возможность подписываться/отписываться на блоги других пользователей.
    FB: Добавил сериализаторы, views.

--- backend/subscriptions/admin.py
+++ backend/subscriptions/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Subscription
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'blog']
+    search_fields = ['user__username', 'blog__user__username']  # Поиск по именам пользователей и блогов
